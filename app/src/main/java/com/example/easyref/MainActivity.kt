b5725a3065bc3b5d87c504870192a22a2c68fa83
Javier de Mena Asenjo <javier.demena@iesdoctorbalmis.com>
2023-05-15 13:53:41 +0200

    Muchos cambios de estilo y barra superior

--- app/src/main/java/com/example/easyref/MainActivity.kt
+++ app/src/main/java/com/example/easyref/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.easyref
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.example.easyref.ViewModel.EasyRefController
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         EasyRefController.iniciarDB(application)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.back)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.getItemId() === android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return false
     }
 }
